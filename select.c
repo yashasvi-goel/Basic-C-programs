de0a2c992aa9f96e744be7b1bde3878316a3a163
yashasvi-goel <31849052+yashasvi-goel@users.noreply.github.com>
2017-12-18 12:06:51 +0530

    Add files via upload

--- select.c
+++ select.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/*
+ * Made by Yashasvi Goel
+ * Enter the string from which words are to be extracted;
+ * the output is enclosed in ':';
+ * Thu Oct 12 09:05:39 IST 2017
+*/
 int main()
 {
 	int t;
