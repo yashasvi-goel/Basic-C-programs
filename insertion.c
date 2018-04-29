dc3882a0b04d38d9235aa6eae0b4ae81872d48d2
yashasvi-goel <31849052+yashasvi-goel@users.noreply.github.com>
2018-04-29 11:55:53 +0530

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -1,40 +1,33 @@
-#include <stdio.h>
 /*
  * Made by Yashasvi Goel
- * Enter the string from which words are to be extracted;
- * the output is enclosed in ':';
+ * Enter the number of numbers you will input;
+ * followed by the same number of integers to be sorted;
+ * the next line contains the sorted output;
  * Thu Oct 12 09:05:39 IST 2017
-*/
-//searching for the right element
+ */
+#include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-	int t;
-//	scanf("%d",&t);
-//	while(t>0)
-	{
-		int n;
-		scanf("%d",&n);
-		int a[n],key;
-		for(int i=0;i<n;i++)
-			scanf("%d",&a[i]);
-		key=a[0];
-//		int temp;
-		for(int i=0;i<n;i++)
-		{
-			key=a[i];
-			for(int j=0;j<i;j++)
-				if(key<a[j])
-				{
-					a[i]=a[i]^a[j];
-					a[j]=a[i]^a[j];
-					a[i]=a[i]^a[j];
-
-				}
-		}
-		for(int i=0;i<n;i++)
-			printf("%d ",a[i]);
-		printf("\n");
-		t--;
-	}
+        int n;
+        scanf("%d",&n);
+        int *a=(int*)malloc(n*sizeof(n));
+        int key;
+        for(int i=0;i<n;i++)
+                scanf("%d",&a[i]);
+        for(int i=0;i<n;i++)    
+        {                                       
+                key=a[i];
+                for(int j=i;j<n;j++)
+                        if(key>a[j])
+                        {                                                                                                                                                             
+				a[i]=a[i]^a[j];
+                                a[j]=a[i]^a[j];                                                                                                                                       
+				a[i]=a[i]^a[j];                                                                                                                                                                      
+			}                                                                 
+        }
+	for(int i=0;i<n;i++)
+		printf("%d ",a[i]);
+        printf("\n");
 	return 0;
 }
