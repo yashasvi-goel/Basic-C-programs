9a19c7f273412bc7880ab4284166eb23a7ad6918
Sahil <sahil.kalamkar@gmail.com>
2019-10-13 17:58:22 +0530

    Added comments to modularExponentiation.cpp

--- modularExponentiation.cpp
+++ modularExponentiation.cpp
@@ -1,9 +1,13 @@
+/*Author: Sahil Kalamkar
+ *Date: 13/10/2019		 
+ */
 #include<bits/stdc++.h>
 
 using namespace std;
 typedef long long ll;
 const int mod = 1e9+7;
 
+//Function to calculate a raised to the power of b modulo m for large values.
 ll modularExponentiation(ll base,ll exponent,ll modulo)
 {
 	if(exponent==0)
@@ -20,10 +24,10 @@ ll modularExponentiation(ll base,ll exponent,ll modulo)
 }
 
 int main()
-{
+{	
+	//Input Format: 3 integers denoting the base,the power and the integer with which you want the remainder.
 	int base,power,m;
 	cin>>base>>power>>m;
-
 	cout<<modularExponentiation(base,power,m)<<'\n';
 	return 0;
 }
